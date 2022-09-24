07db7e11af4cfffa10f7f42833530bdfd90c23cb
Erick Rodríguez <e12antonio@gmail.com>
2022-09-23 19:09:24 -0600

    other description in orders again

--- src/pages/myorder.js
+++ src/pages/myorder.js
@@ -149,7 +149,7 @@ const Myorder = () => {
                   </div>
                 )}
               </div>
-              <p className="font-semibold mt-2">Al realizar el <b className="font-semibold">Deposito</b> tomarle tomarle una foto a tu boleta de pago. Si ha sido <b className="font-semibold">Transferencia Bancaria</b> manda una captura de pantalla  de la transferencia bancaria y agregala:</p>
+              <p className="font-semibold mt-2">Al realizar el <b className="font-bold">Deposito</b> tomarle tomale fóto a tu boleta de pago. Si ha sido <b className="font-bold">Transferencia Bancaria</b> manda una captura de pantalla  de la transferencia bancaria y agregala:</p>
               <label
                 className={`my-2 w-full h-[80px] ${
                   imgAdded ? 'bg-pink-800 hover:bg-pink-700' : 'bg-slate-800 hover:bg-slate-700'
